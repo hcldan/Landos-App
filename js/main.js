e441ffd8330a91845593b004d04618d973a290c9
Robert O'Neill <rjroneill@gmail.com>
2012-10-24 13:47:33 -0400

    Added paths in requirejs config to support running the app in a gadget.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 
-require.config({
-  paths: {
-    jquery: 'libs/jquery',
-    underscore: 'libs/underscore',
-    backbone: 'libs/backbone',
-    hogan: 'libs/hogan',
-    io: 'libs/io'
-  }
-
-});
-
 require([
   'app'
 ], function(App){
+  debugger;
   App.initialize();
 });
