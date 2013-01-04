19a4d3bfc68fbd271314df130a0b1b4f8c4dedc8
Dan Dumont <ddumont@us.ibm.com>
2013-01-04 10:53:40 -0500

    remove illegal characters

--- src/main/webapp/landos/GadgetContainer.js
+++ src/main/webapp/landos/GadgetContainer.js
@@ -4,7 +4,7 @@ define('landos/GadgetContainer', [
   'dojo/_base/declare',
   'dijit/_WidgetBase',
   'dijit/_Container',
-  'dijit/_TemplatedMixin','
+  'dijit/_TemplatedMixin'
 ], function(require, lang, declare, _WidgetBase, _Container, _TemplatedMixin) {
   return declare([_WidgetBase, _Container, _TemplatedMixin], {
     templateString:
