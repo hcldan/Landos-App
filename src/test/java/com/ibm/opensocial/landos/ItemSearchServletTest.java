caba263e24120f0a93f393496295ed51108ff8cf
Dan Dumont <ddumont@us.ibm.com>
2013-01-11 17:55:43 -0500

    Fix errors in item search tests

--- src/test/java/com/ibm/opensocial/landos/ItemSearchServletTest.java
+++ src/test/java/com/ibm/opensocial/landos/ItemSearchServletTest.java
@@ -5,17 +5,20 @@ import static org.junit.Assert.assertEquals;
 import javax.servlet.http.HttpServletRequest;
 
 import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ItemSearchServletTest extends EasyMock { 
   private ItemSearchServlet servlet;
+  private IMocksControl control;
   private HttpServletRequest req;
   
   @Before
   public void before() throws Exception {
     servlet = new ItemSearchServlet();
+    control = createControl();
   }
   
   @After
@@ -25,11 +28,11 @@ public class ItemSearchServletTest extends EasyMock {
   
   @Test
   public void testGetLastSegmentPath() throws Exception {
-    req = TestUtils.mockRequest(null, null, "/foo/bar");
+    req = TestControlUtils.mockRequest(control, null, null, "/foo/bar");
     
-    replay(req);
-    
-    verify(req);
+//    control.replay();
+//    
+//    control.verify();
   }
 }
 
