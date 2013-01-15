f563e963dd9469c9b45d91c9e058c27aa2ae7cbb
Dan Dumont <ddumont@us.ibm.com>
2013-01-14 20:36:01 -0500

    call back with the search results.

--- src/main/webapp/landos/FilteringSelect.js
+++ src/main/webapp/landos/FilteringSelect.js
@@ -22,16 +22,19 @@ define([
       this._lastQuery = text;
       if (text.length > 2) {
         this._queryDeferHandle = this.defer(lang.hitch(this, function(text) {
-          var fetch = this._fetchHandle = new Deferred();
+          var fetch = this._fetchHandle = new Deferred(),
+              params = { format: 'json', href: landos.getAPIUri('items') + '/' + encodeURIComponent(text) },
+              query = {};
           
-          var params = { format: 'json', href: landos.getAPIUri('items') + '/' + encodeURIComponent(text) };
+          query[this.searchAttr] = text;
           osapi.http.get(params).execute(lang.hitch(this, function(result) {
             fetch[result.error || result.status != 200 ? 'reject' : 'resolve'](result);
           }));
           
           fetch.then(lang.hitch(this, function(results) {
             this._fetchHandle = null;
-            
+            var matches = results && results.content && results.content.matches || [];
+            this.onSearch(matches, query, {});            
           })).otherwise(lang.hitch(this, function(error) {
             this._fetchHandle = null;
             
