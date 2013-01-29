110578a9cd7a28f0ba1df6881eac41ce97e6f183
Dan Dumont <ddumont@us.ibm.com>
2013-01-29 14:17:24 -0500

    Create a data grid in the run display to show the data.

--- src/main/webapp/landos/RunSummary.js
+++ src/main/webapp/landos/RunSummary.js
@@ -6,22 +6,35 @@ define([
   'landos/base/LazyContainer',
   'dojo/Deferred'
 ], function(require, landos, lang, declare, LazyContainer, Deferred) {
+  var undef;
   return declare(LazyContainer, {
     title: 'Run Summary',
+    runid: undef,
+    
+    // Attached via template
+    _grid: undef,
     
     constructor: function(runid) {
-      
+      this.runid = runid;
     },
     
     getRealTemplateString: function() {
       var def = new Deferred();
       require([  
-
+        'dojox/grid/DataGrid'
       ], function() {
         def.resolve(
             '<div data-dojo-attach-point="containerNode">'
-
-          + '</div>'      
+          +   '<table data-dojo-attach-point="_grid" data-dojo-type="dojox/grid/DataGrid">'
+          +     '<thead>'
+          +       '<tr>'
+          +         '<th field="displayItem">Item</th>'
+          +         '<th field="who" width="200px">For</th>'
+          +         '<th field="paid" width="75px">Paid</th>'
+          +       '</tr>'
+          +     '</thead>'
+          +   '</table>'
+          + '</div>'
         );
       });
       return def.promise;
